f19e1bfa3ed68965171434808a93ce0c10bec20d
Sebastian Wolf <sebxwolf@gmail.com>
2020-04-23 23:36:50 +0200

    updated .py notebook

--- cdr-aggregation/notebooks/agregation_offsite.py
+++ cdr-aggregation/notebooks/agregation_offsite.py
@@ -2,27 +2,27 @@
 # coding: utf-8
 
 # # Production of indicators for the COVID19 Mobility Task Force
-#
+# 
 # In this notebook we produce indicators for the [COVID19 Mobility Task Force](https://github.com/worldbank/covid-mobile-data).
-#
+# 
 # [Flowminder](https://covid19.flowminder.org) indicators are produced to increase the availability of comparable datasets across countries, and have been copied without modification from the [Flowminder COVID-19 github repository](https://github.com/Flowminder/COVID-19) (except for the start and end dates). These have been supplemented by a set of *priority* indicators with data for ingestion into the dashboard in this repository.
-#
+# 
 # In this notebook we produce indicators in the following four steps:
-#
+# 
 # - **Import code**: The code for the aggregation is included in the 'custom_aggregation' and 'flowminder_aggregation' scripts
-# - **Import data**:
-# To set up the data import we need to place the CDR data files into the `data/new/CC/telco/` folder, where we replace `CC` with the country code and `telco` with the company abbreviation.
+# - **Import data**: 
+# To set up the data import we need to place the CDR data files into the `data/new/CC/telco/` folder, where we replace `CC` with the country code and `telco` with the company abbreviation. 
 # We also need to place csv files with the tower-region mapping and distance matrices into the `data/support-data/CC/telco/geofiles` folder, and then modify the `data/support_data/config_file.py` to specify:
-#     - *geofiles*: the names of the geofiles,
+#     - *geofiles*: the names of the geofiles, 
 #     - *country_code*: country code and company abbreviation,
 #     - *telecom_alias*: the path to the `data` folder,
 #     - *data_paths*: the names to the subfolders in `data/new/CC/telco/` that hold the csv files. Simply change this to `[*]` if you didn't create subfolders and want to load all files.
-#
+#     - *dates*: set the start and end date of the data you want to produce the indicators for.
+#     
 # Find more information about the `config_file.py` settings see the [github page](https://github.com/worldbank/covid-mobile-data/tree/master/cdr-aggregation).
-#
-# - **Set dates**: Set the start and end date of the data you want to produce the indicators for.
+#     
 # - **Run aggregations**: By default, we produce all flowminder and priority indicators. We've included 4 re-tries in case of failure, which we have experienced to help on databricks but is probably irrelevant in other settings. Note that before you can re-run these aggregations, you need to move the csv outputs that have been saved in `data/results/CC/telco/` in previous runs to another folder, else these indicators will be skipped. This prevents you from accidentally overwriting previous results. This way you can also delete the files only for the indicators you want to re-produce, and skip any indicatos you don't want to re-produce.
-#
+# 
 # The outcome of this effort will be used to inform policy making using a [mobility indicator dashboard](https://github.com/worldbank/covid-mobile-data/tree/master/dashboard-dataviz).
 
 # # Import code
@@ -30,8 +30,8 @@
 # In[ ]:
 
 
-# get_ipython().run_line_magic('load_ext', 'autoreload')
-# get_ipython().run_line_magic('autoreload', '2')
+# %load_ext autoreload
+# %autoreload 2
 
 
 # In[ ]:
@@ -110,12 +110,12 @@ ds.load_geo_csvs()
 
 ## Use this in case you want to cluster the towers and create a distance matrix
 
-ds.create_gpds()
-from modules.tower_clustering import *
-clusterer = tower_clusterer(ds, 'admin2', 'ID_2')
-ds.admin2_tower_map, ds.distances = clusterer.cluster_towers()
-clusterer = tower_clusterer(ds, 'admin3', 'ADM3_PCODE')
-ds.admin3_tower_map, ds.distances  = clusterer.cluster_towers()
+# ds.create_gpds()
+# from modules.tower_clustering import *
+# clusterer = tower_clusterer(ds, 'admin2', 'ID_2')
+# ds.admin2_tower_map, ds.distances = clusterer.cluster_towers()
+# clusterer = tower_clusterer(ds, 'admin3', 'ADM3_PCODE')
+# ds.admin3_tower_map, ds.distances  = clusterer.cluster_towers()
 
 
 # In[ ]:
@@ -123,24 +123,9 @@ ds.admin3_tower_map, ds.distances  = clusterer.cluster_towers()
 
 ## Use this in case you want to create a voronoi tesselation
 
-from modules.voronoi import *
-voronoi = voronoi_maker(ds, 'admin3', 'ADM3_PCODE')
-ds.voronoi = voronoi.make_voronoi()
-
-
-# # Set dates
-#
-# These dates need to indicate which period of data the indicator should be created for:
-#
-# - **start_date**: This matters mainly for the computation of home locations. For now, we haven't implemented loading of previously saved home locations, therefore it is recommended to leave this unchanged for now.
-# - **end_date**: This needs to be set to the day the dataset ends.
-
-# In[ ]:
-
-
-dates = {'start_date' : dt.datetime(2020,2,1),
-         'end_date' : dt.datetime(2020,3,31)}
-dates = add_week_dates(dates)
+# from modules.voronoi import *
+# voronoi = voronoi_maker(ds, 'admin3', 'ADM3_PCODE')
+# ds.voronoi = voronoi.make_voronoi()
 
 
 # # Run aggregations
@@ -152,8 +137,7 @@ dates = add_week_dates(dates)
 
 agg_flowminder = aggregator(result_stub = '/admin2/flowminder',
                             datasource = ds,
-                            regions = 'admin2_tower_map',
-                            dates = dates)
+                            regions = 'admin2_tower_map')
 
 agg_flowminder.attempt_aggregation()
 
@@ -165,8 +149,7 @@ agg_flowminder.attempt_aggregation()
 
 agg_flowminder = aggregator(result_stub = '/admin3/flowminder',
                             datasource = ds,
-                            regions = 'admin3_tower_map',
-                            dates = dates)
+                            regions = 'admin3_tower_map')
 
 agg_flowminder.attempt_aggregation()
 
@@ -178,8 +161,7 @@ agg_flowminder.attempt_aggregation()
 
 agg_custom = custom_aggregator(result_stub = '/admin2/custom',
                                datasource = ds,
-                               regions = 'admin2_tower_map',
-                               dates = dates)
+                               regions = 'admin2_tower_map')
 
 agg_custom.attempt_aggregation()
 
@@ -191,7 +173,7 @@ agg_custom.attempt_aggregation()
 
 agg_custom = custom_aggregator(result_stub = '/admin3/custom',
                             datasource = ds,
-                            regions = 'admin3_tower_map',
-                            dates = dates)
+                            regions = 'admin3_tower_map')
 
 agg_custom.attempt_aggregation()
+
