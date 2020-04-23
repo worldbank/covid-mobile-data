281825c09aca4775ec7febab6e2a18cd11965d46
LeonardoViotti <leonardoviotti@gmail.com>
2020-04-23 19:30:08 -0400

    Added all the layerstto riskcoropleth draft

--- dashboard-dataviz/zimbabwe_dashboard/riskcoropleth_draft.R
+++ dashboard-dataviz/zimbabwe_dashboard/riskcoropleth_draft.R
@@ -1,6 +1,8 @@
 # Draft risk coropleth
 
 library(leaflet)
+library(wesanderson)
+
 
 # Load Spatial Data ------------------------------------------------------
 
@@ -29,7 +31,7 @@ pal2 <- colorBin("YlOrRd",
 
 
 
-layers <- c("mean_hiv_pop_weighted_cat",       
+vars <- c("mean_hiv_pop_weighted_cat",       
             "mean_anaemia_pop_weighted_cat",   
             "mean_overweight_pop_weighted_cat",
             "mean_smoker_pop_weighted_cat",    
@@ -37,28 +39,55 @@ layers <- c("mean_hiv_pop_weighted_cat",
             "severe_covid_risk"  
             )
 
+groups <- c("HIV prevalence quintile", 
+            "Anaemia prevalence quintile",
+            "Respiratory illness prevalence quintile",
+            "Overweight prevalence quintile", 
+            "Smoking prevalence quintile",
+            "Severe COVID-19 risk")
+
+group_df <- 
+  data.frame(var = vars,
+             group = groups,
+             stringsAsFactors = F)
+
+
 # Add Layer custom function
-add_cus_layer  <- function(map, var,
-                           pal = pal2){
+add_cus_layer  <- function(map,
+                           var,
+                           # pal,
+                           gdf = group_df){
   
   pal <- colorBin("YlOrRd", 
                   domain = a2@data[[var]])
   
+  nmap <- 
   addPolygons(map,
     fillColor = ~pal(a2@data[[var]]),
     weight = 2,
     opacity = 1,
     color = "white",
     # dashArray = "3",
-    group = var,
-    fillOpacity = 0.7)
+    group = gdf$group[gdf$var == var],
+    fillOpacity = 0.7) #%>% 
+    # addLegend(title = gdf$group[gdf$var == var],
+    #           position = 'topleft',
+    #           colors = pal(1:5),
+    #           labels = 1:5,
+    #           group = gdf$group[gdf$var == var])
+    
+  
+  return(nmap)
+  
 }
+
 
 
 # Set map boundaries
 map_extent <- a2 %>% extent()
 
 # Create map
+# map <- 
 leaflet(a2) %>%
   addProviderTiles(providers$OpenStreetMap.Mapnik) %>%
   fitBounds(
@@ -67,14 +96,14 @@ leaflet(a2) %>%
     lng2 = map_extent@xmax,
     lat2 = map_extent@ymax
   ) %>% 
-  add_cus_layer(var = layers[1]) %>% 
-  add_cus_layer(var = layers[2]) %>% 
-  add_cus_layer(var = layers[3]) %>% 
-  add_cus_layer(var = layers[4]) %>% 
-  add_cus_layer(var = layers[5]) %>% 
-  add_cus_layer(var = layers[6]) %>% 
+  add_cus_layer(var = group_df$var[1]) %>% 
+  add_cus_layer(var = group_df$var[2]) %>% 
+  add_cus_layer(var = group_df$var[3]) %>% 
+  add_cus_layer(var = group_df$var[4]) %>% 
+  add_cus_layer(var = group_df$var[5]) %>% 
+  add_cus_layer(var = group_df$var[6]) %>% 
   
 addLayersControl(
-  baseGroups = layers,
+  baseGroups = group_df$group,
   options = layersControlOptions(collapsed = FALSE)
   )
