ccf994ddfaa16ca6602a45a401fdc0250a15bb9c
LeonardoViotti <leonardoviotti@gmail.com>
2020-05-11 12:40:03 -0400

    Bug fixes in data_files_comparisson.py

--- data-checks/data_files_comparisson.py
+++ data-checks/data_files_comparisson.py
@@ -19,12 +19,8 @@ ICUST = IRESULTS + "custom/"
 
 filenames = os.listdir(IFLOW)
 
-file_i = filenames[0]
-# Our file
-d1 = pd.read_csv(FLOWM_adm3_path + file_i) 
-
-# I's file
-d2 = pd.read_csv(IFLOW + file_i) 
+# Custom indicatos
+# filenames = os.listdir(ICUST)
 
 #-----------------------------------------------------------------#
 # Make sure data is compatible
@@ -47,25 +43,20 @@ def compat(data,
         new_data = new_data.sort_values( by = [new_data.columns[0], new_data.columns[1], new_data.columns[2] ])
     return new_data
 
-for i in range(0, len(filenames)-1):
-    file_i = filenames[i]
-    # print(i)
-    # print(filenames[i])
-    # Our file
-    d1 = pd.read_csv(FLOWM_adm3_path + file_i) 
-    # I's file
-    d2 = pd.read_csv(IFLOW + file_i) 
+
+# Comparisson outputs function
+def compare_dfs(df1,df2, filename):
     # Set time var (GARMBIARRA WARNING)
-    time = list(d1.columns[list(d1.columns.str.contains('date'))])
+    time = list(df1.columns[list(df1.columns.str.contains('date'))])
     # Process data to be in the same format
-    d1 = compat(d1, timevar = time)
-    d2 = compat(d2, timevar = time)
+    df1 = compat(df1, timevar = time)
+    df2 = compat(df2, timevar = time)
     # Merge dfs
-    index_cols = list(d1.columns[0:-1])
+    index_cols = list(df1.columns[0:-1])
     #Make sure merging columns are str
-    d1[index_cols] = d1[index_cols].astype(str)
-    d2[index_cols] = d2[index_cols].astype(str)
-    cdf = d1.merge(d2, left_on = index_cols, right_on = index_cols)
+    df1[index_cols] = df1[index_cols].astype(str)
+    df2[index_cols] = df2[index_cols].astype(str)
+    cdf = df1.merge(df2, left_on = index_cols, right_on = index_cols)
     #--------------------#
     # Calculate differeces
     # Proportion of mismatches
@@ -77,12 +68,42 @@ for i in range(0, len(filenames)-1):
     # Average difference
     avg_diff = str(round(cdf['pdiff'].mean(skipna = True), 4)*100)
     # Print report
-    print(i)
-    print(filenames[i])
-    print('N rows ours: ' + str(d1.shape[0]) )
-    print("N rows Isaac's: " + str(d2.shape[0]))
+    print(filename)
+    print('N rows ours: ' + str(df1.shape[0]) )
+    print("N rows Isaac's: " + str(df2.shape[0]))
     print('Of matching rows:')
     print(' - Average difference of count column: ' + avg_diff + "%")
     print(' - Percentage rows that are different: ' + p_rows_diff + "%")
     print('\n')
 
+
+# Custom indicatos csv
+i1 = pd.read_csv(I1_Adm3_path + 'transactions_per_hour.csv')
+i1i = pd.read_csv(ICUST + 'transactions_per_hour.csv')
+
+i1 = compat(i1, timevar = [])
+i1i = compat(i1i, timevar = [])
+
+index_cols = list(df1.columns[0:-1])
+df1[index_cols] = df1[index_cols].astype(str)
+df2[index_cols] = df2[index_cols].astype(str)
+cdf = df1.merge(df2, left_on = index_cols, right_on = index_cols)
+
+
+compare_dfs(i1,i1i, filename = 'transactions_per_hour.csv')
+
+
+# Flowminder csvs
+for i in range(0, len(filenames)-1):
+    file_i = filenames[i]
+    # print(i)
+    # print(filenames[i])
+    # Our file
+    d1 = pd.read_csv(FLOWM_adm3_path + file_i) 
+    # I's file
+    d2 = pd.read_csv(IFLOW + file_i) 
+    
+    # Run comparisson
+    print(i)
+    print(filenames[i])
+    compare_dfs(d1,d2)
