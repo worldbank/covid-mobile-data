185fa15398b0d2180877037db927a8566764ee5f
LeonardoViotti <leonardoviotti@gmail.com>
2020-05-14 10:36:21 -0400

    Uptates to data-checks.

--- data-checks/MASTER.py
+++ data-checks/MASTER.py
@@ -12,7 +12,6 @@
 DATA_path = "C:/Users/wb519128/WBG/Sveta Milusheva - COVID 19 Results/"
 DATA_POC = DATA_path + "proof-of-concept/"
 
-
 DATA_DB_raw_indicators = DATA_POC + "databricks-results/zw/"
 DATA_dashboad_clean = DATA_POC + "/files_for_dashboard/files_clean/"
 
@@ -32,15 +31,23 @@ I2_path = DATA_DB_raw_indicators + "indicator 2/"
 I2_Adm3_path = I2_path + "admin3/"
 I2_towercluster_path = I2_path + "tower_cluster/"
 
+
+# Unique subscribers per day
+I3_path = DATA_DB_raw_indicators + "indicator 3/"
+I3_Adm3_path = I3_path + "admin3/"
+
+
 # OD matrix
 I5_path = DATA_DB_raw_indicators + "indicator 5/"
 I5_Adm2_path = I5_path + "admin2/"
 I5_Adm3_path = I5_path + "admin3/"
 
+#---------------------#
 # Flowminder indicators
 FLOWM_path = DATA_DB_raw_indicators + "flowminder indicators/"
 FLOWM_adm2_path = FLOWM_path + "admin2/"
 FLOWM_adm3_path = FLOWM_path + "admin3/"
+
 
 #---------------#
 # Outputs
--- data-checks/data_files_comparisson.py
+++ data-checks/data_files_comparisson.py
@@ -6,6 +6,9 @@ import os
 import re
 import numpy as np
 import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt 
+# import seaborn as sns
 from datetime import datetime
 
 
@@ -27,8 +30,8 @@ filenames = os.listdir(IFLOW)
 
 # Masure order and date formats are the same
 def compat(data,
+           timevar,
            standardize_time = False,
-           timevar = time,
            regvar = 'region'):
     new_data = data
     
@@ -45,7 +48,7 @@ def compat(data,
 
 
 # Comparisson outputs function
-def compare_dfs(df1,df2, filename):
+def compare_dfs(df1,df2, filename = None, outputdf = False):
     # Set time var (GARMBIARRA WARNING)
     time = list(df1.columns[list(df1.columns.str.contains('date'))])
     # Process data to be in the same format
@@ -67,32 +70,21 @@ def compare_dfs(df1,df2, filename):
                     cdf[cdf.columns[-2]])/cdf[cdf.columns[-2]])
     # Average difference
     avg_diff = str(round(cdf['pdiff'].mean(skipna = True), 4)*100)
-    # Print report
-    print(filename)
-    print('N rows ours: ' + str(df1.shape[0]) )
-    print("N rows Isaac's: " + str(df2.shape[0]))
-    print('Of matching rows:')
-    print(' - Average difference of count column: ' + avg_diff + "%")
-    print(' - Percentage rows that are different: ' + p_rows_diff + "%")
-    print('\n')
+    
+    if outputdf:
+        return(cdf)
+    else:
+        # Print report
+        print(filename)
+        print('N rows ours: ' + str(df1.shape[0]) )
+        print("N rows Isaac's: " + str(df2.shape[0]))
+        print('Of matching rows:')
+        print(' - Average difference of count column: ' + avg_diff + "%")
+        print(' - Percentage rows that are different: ' + p_rows_diff + "%")
+        print('\n')
 
 
-# Custom indicatos csv
-i1 = pd.read_csv(I1_Adm3_path + 'transactions_per_hour.csv')
-i1i = pd.read_csv(ICUST + 'transactions_per_hour.csv')
-
-i1 = compat(i1, timevar = [])
-i1i = compat(i1i, timevar = [])
-
-index_cols = list(df1.columns[0:-1])
-df1[index_cols] = df1[index_cols].astype(str)
-df2[index_cols] = df2[index_cols].astype(str)
-cdf = df1.merge(df2, left_on = index_cols, right_on = index_cols)
-
-
-compare_dfs(i1,i1i, filename = 'transactions_per_hour.csv')
-
-
+#-----------------------------------------------------------------#
 # Flowminder csvs
 for i in range(0, len(filenames)-1):
     file_i = filenames[i]
@@ -107,3 +99,109 @@ for i in range(0, len(filenames)-1):
     print(i)
     print(filenames[i])
     compare_dfs(d1,d2)
+    
+#-----------------------------------------------------------------#    
+# Custom indicatos csv
+
+# Indicator 1 #
+
+i1 = pd.read_csv(I1_Adm3_path + 'transactions_per_hour.csv')
+i1i = pd.read_csv(ICUST + 'transactions_per_hour.csv')
+
+# i1 = compat(i1, timevar = [])
+# i1i = compat(i1i, timevar = [])
+
+cdf = compare_dfs(i1,i1i, outputdf = True)
+cdf["diff_flag"] = cdf["count_x"] != cdf["count_y"]
+cdf['date'] = pd.to_datetime(cdf['hour']).dt.date
+
+
+foo = cdf[cdf['diff_flag']]
+
+foo.to_csv('C:/Users/wb519128/Desktop/i1_differences.csv',
+           index = False)
+
+# Indicator 3 #
+
+i3 = pd.read_csv(I3_Adm3_path + 'unique_subscribers_per_day.csv')
+i3i = pd.read_csv(ICUST + 'unique_subscribers_per_day.csv')
+
+cdf3 = compare_dfs(i3,i3i, outputdf = True)
+cdf3["diff_flag"] = cdf3["count_x"] != cdf3["count_y"]
+cdf3['day'] = pd.to_datetime(cdf3['day']).dt.date
+
+
+cdf3[cdf3['day'] == dt.date(2020, 2, 3)]
+
+foo = cdf3[cdf3['diff_flag']]
+
+foo.to_csv('C:/Users/wb519128/Desktop/i3_differences.csv',
+           index = False)
+
+
+
+# Indicator 5 #
+I5_Adm3_path
+
+i5 = pd.read_csv(I5_Adm3_path + 'origin_destination_connection_matrix_per_day.csv')
+i5i = pd.read_csv(ICUST + 'origin_destination_connection_matrix_per_day.csv')
+
+#cdf5 = compare_dfs(i5,i5i, outputdf = True)
+#cdf5["diff_flag"] = cdf5["total_count_x"] != cdf5["total_count_y"]
+
+compare_dfs(i5,i5i, outputdf = False)
+
+bar = i5.merge(i5i, on = ['connection_date', 'region_from', 'region_to'])
+bar["diff_flag"] = bar["od_count_x"] != bar["od_count_y"]
+
+diff_day_df = bar.groupby('connection_date').sum()
+diff_day_df = diff_day_df.reset_index()
+
+diff_day_df['day'] = pd.to_datetime(diff_day_df['connection_date']).dt.day
+
+plt.plot('day',
+         'diff',
+         data = diff_day_df)
+
+# set(bar['connection_date'])
+# len(set(pd.to_datetime(foo['connection_date']).dt.date))
+# len(set(foo['region_from']))
+
+# # Absolute difference by day
+# bar['diff'] = bar['od_count_x']- bar['od_count_y'] 
+
+
+
+# foo = bar[bar['diff_flag']]
+
+# foo['diff'] = foo['od_count_x']- foo['od_count_y'] 
+# foo['diff'].mean()
+
+export_i5_merged = bar.rename(
+    columns = {
+        'subscriber_count_x' : 'subscriber_count',
+        'subscriber_count_y' : 'subscriber_count_isaac',
+        'od_count_x': 'od_count_x',
+        'od_count_y': 'od_count_isaac',
+        'total_count_x' : 'total_count',
+        'total_count_y' : 'total_count_isaac'})
+
+
+
+export_i5_merged\
+    .to_csv('C:/Users/wb519128/Desktop/i5_merged_with_Isaacs.csv',
+           index = False)
+
+
+
+
+#-----------------------------------------------------------------#    
+# DRAFT
+
+file_i = filenames[0]
+
+d1 = pd.read_csv(FLOWM_adm3_path + file_i) 
+d2 = pd.read_csv(IFLOW + file_i) 
+
+cdf = compare_dfs(d1,d2, outputdf = True)
+cdf["diff_flag"] = cdf["count_x"] != cdf["count_y"]
