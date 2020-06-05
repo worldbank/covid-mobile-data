a1e2b2023dfad18df65885bf31c6473556afb84a
LeonardoViotti <leonardoviotti@gmail.com>
2020-06-05 16:49:47 -0400

    Data checks i1 hourly counts by hour.

--- data-checks/i1_admin2_DRAFT.py
+++ data-checks/i1_admin2_DRAFT.py
@@ -9,6 +9,10 @@ EXPORT = False
 import os
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
+
 import seaborn as sns; sns.set()
 
 
@@ -36,56 +40,96 @@ i1_agg['hour'] = i1_agg['hour'].astype('datetime64')
 i1_agg['district_id'] = i1_agg['district_id'].astype('int')
 
 #-----------------------------------------------------------------#
-# Plots
+# Transactions per hour by district line plot.
 
-def region_plt(reg_i,
+# Line plot function definition
+def line_plot(reg_i,
                var = 'count_p',
                data = i1_agg,
                region = 'district_id',
                region_str = 'district_name',
                time = 'hour'):
-    # Select regions to be in the plot
-    plt_data = data[data[region] == reg_i].set_index(time)
-    # Reshape dat to plot
-    # splt_data = pd.pivot_table(plt_data.reset_index(),
-    #                            index=time,
-    #                            columns=region, 
-    #                            values=var)
-    # Plot
-    title = plt_data[region_str][0]
-    plot = plt_data[var].plot(color = 'navy',
-                              title = title,
-                              figsize=(12,8))
-    #
-    return plot
+    plt_data = data[data[region] == reg_i]
+    fig = go.Figure()
+    # Create line
+    fig.add_trace(go.Scatter(x=plt_data[time], y=plt_data[var],
+                    mode='lines',
+                    name='lines'))
+    # Additional formatting 
+    title =  str(plt_data[region].iloc[0]) + plt_data[region_str].iloc[0]
+    fig.update_layout(
+        title=title,
+        xaxis_title="Time",
+        yaxis_title="Count",
+        font=dict(
+            # family="Courier New, monospace",
+            size=18,
+            color="#7f7f7f"),
+        autosize=False,
+        width=1200,
+        height=700
+        )
+    return(fig)
 
 # Districts list
 dists = list(set(i1_agg['district_id']))
 
+# region_plt(d)
+# plt.show()
+
 # Loop over districts
 for d in dists:
     print(d)
+    # Create plot
+    plt_i = line_plot(d)
     # Export
     save_name = None
     save_name = 'i1_districts_count' + str(d) + '.png'
-    region_plt(d).figure.savefig(OUT_plots + 'daily_obs_region/' + save_name,  dpi=250)
+    plt_i.write_image(OUT_plots + 'daily_obs_region/' + save_name)
 
 
-plt_i = region_plt(dists[0])
-save_name = 'i1_districts_count' + str(dists[0]) + '.png'
-plt_i.figure.savefig(OUT_plots + 'daily_obs_region/' + save_name,  dpi=250)
+#-----------------------------------------------------------------#
+# Transactions per hour by day. That is one plot per hour
+i1_agg['time'] = pd.to_datetime(i1_agg['hour']).dt.hour
+i1_agg['date'] = pd.to_datetime(i1_agg['hour']).dt.date
 
-# # Loop 5 by 5 and save plots
-# loop_list = list(range(5,len(dists),5))
-# loop_list.append(len(dists))
-# last_i = 0
-# idx=0
-# for i in loop_list:
-#     dist_idx =range(last_i, i+1)
-#     save_name = 'i1_districts_count' + str(idx) + '.png'
-#     region_plt(regions = dists[last_i:i+1])[0].get_figure().savefig(OUT_hfcs + save_name,  dpi=250)
-#     last_i = i
-#     idx += 1
+
+def hourly_scatter(reg_i,
+               var = 'count_p',
+               data = i1_agg,
+               region = 'district_id',
+               region_str = 'district_name',
+               time = 'date',
+               facets = 'time'):
+    # Subset data
+    plt_data = data[data[region] == reg_i]
+    # Create plot
+    fig = px.scatter(plt_data, 
+                    x= time, 
+                    y = var, 
+                    facet_col = facets, 
+                    facet_col_wrap = 5,
+                    width=1200,
+                    height=700)
+    # Additional formatting 
+    title =  str(plt_data[region].iloc[0]) + ' - ' + plt_data[region_str].iloc[0]
+    fig.update_layout(title_text= title)
+    fig.update_yaxes(matches=None)
+    fig.for_each_annotation(lambda a: a.update(text=a.text.replace("time=", "")))
+    # Format axis titles
+    return(fig)
+
+# Loop over districts
+for d in dists:
+    print(d)
+    # Create plot
+    plt_i = hourly_scatter(d)
+    # Export
+    save_name = None
+    save_name = 'i1_hourly_obs_byhour' + str(d) + '.png'
+    plt_i.write_image(OUT_plots + 'hourly_obs_by_hour_region/' + save_name)
+
+
 
 
 #-----------------------------------------------------------------#
@@ -94,3 +138,7 @@ if EXPORT:
     i1_agg.to_csv(OUT_hfcs + 'Sheet comp panel/i1_admin2.csv', index = False)
 
 
+
+
+#-----------------------------------------------------------------#
+# DRAFT
