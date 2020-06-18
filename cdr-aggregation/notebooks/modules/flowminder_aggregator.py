23f45483ad3c13f0f947dca9235262d625fa8003
Sebastian Wolf <sebxwolf@gmail.com>
2020-06-18 10:17:37 +0200

    cholera indicator

--- cdr-aggregation/notebooks/modules/flowminder_aggregator.py
+++ cdr-aggregation/notebooks/modules/flowminder_aggregator.py
@@ -27,42 +27,12 @@ class flowminder_aggregator(aggregator):
 
     Methods
     -------
-    create_sql_dates()
-        Convert the dates to strings to be used in the flowminder sql queries
+    run_and_save_all(table_name)
+        runs run_and_save on the list of all flowminder queries at once
 
-    create_view(df, table_name)
-        Creates a view of a dataframe
-
-    save(table_name)
-      Repartitions a dataframe into a single partition and writes it to a csv file
-
-    save_and_report(table_name)
-        Checks whether csv file exists before saving table_name to csv
-
-    run_and_save_sql(table_name)
-        - programmatically runs an sql query and produces a dataframe
-        - saves the result to a csv
-        - creates a view
-
-    run_and_save_all_sql(table_name)
-        runs run_and_save_sql on the list of all flowminder queries at once
-
-    run_save_and_rename_all_sql()
-        runs run_and_save_all_sql and then renames the csv files created and
+    run_save_and_rename_all()
+        runs run_and_save_all and then renames the csv files created and
         moves them to their parent folder
-
-    rename_csv(table_name)
-        - rename a specific csv
-        - move a csv to parent folder, rename it, then delete its remaining folder
-
-    rename_all_csvs(table_name)
-        renames all csvs at once
-
-    rename_if_not_existing(table_name)
-        rename only if the file doesn't exist as csv yet, handles errors
-
-    check_if_file_exists(table_name)
-        checks whether a csv exists before we re-create
 
     attempt_aggregation(indicators_to_produce = 'all', no_of_attempts = 4)
         - attempts aggregation of all flowminder indicators
@@ -89,13 +59,6 @@ class flowminder_aggregator(aggregator):
         # initiate with parent init
         super().__init__(result_stub,datasource,regions)
 
-    def create_sql_dates(self):
-        self.dates_sql = {'start_date' : "\'" + self.dates['start_date'].isoformat('-')[:10] +  "\'",
-                          'end_date' :  "\'" + self.dates['end_date'].isoformat('-')[:10] +  "\'",
-                          'start_date_weeks' :  "\'" + self.dates['start_date_weeks'].isoformat('-')[:10] +  "\'",
-                          'end_date_weeks' : "\'" + self.dates['end_date_weeks'].isoformat('-')[:10] +  "\'"}
-
-
     def run_and_save_all(self):
       for table_name in self.table_names:
           df = self.spark.sql(self.sql_code[table_name])
@@ -104,7 +67,6 @@ class flowminder_aggregator(aggregator):
     def run_save_and_rename_all(self):
       self.run_and_save_all()
       self.rename_all_csvs()
-
 
     def attempt_aggregation(self, indicators_to_produce = 'all', no_of_attempts = 4):
         attempts = 0
