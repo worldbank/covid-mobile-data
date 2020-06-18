b19ebc87e031ef4f28d9637dd8f666341035c244
Sebastian Wolf <sebxwolf@gmail.com>
2020-06-18 10:47:43 +0200

    Dropped retry attempts, as it doesnt solve our Databricks auth issue and wastes resources. Increased commenting of attempt aggregation

--- cdr-aggregation/notebooks/modules/custom_aggregator.py
+++ cdr-aggregation/notebooks/modules/custom_aggregator.py
@@ -35,7 +35,9 @@ class custom_aggregator(priority_aggregator):
                 pass
 
 
-##### Non-priority Indicators - not used at the moment, but kept just in case
+##### Non-priority Indicators - not used at the moment, but kept just in case.
+# Mostly for internal use by the WB Covid19 Task Force, hence documentation is
+# not yet very detailed.
 
     def origin_destination_matrix(self, time_filter, frequency):
       result = self.df.where(time_filter)\
