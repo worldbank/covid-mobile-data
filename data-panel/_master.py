4ad55ddd7d56c1cb72d4a2b5f06ae5e3e37c62fa
LeonardoViotti <leonardoviotti@gmail.com>
2020-07-09 18:35:29 -0400

    Indicators panel clean first version.

--- data-panel/_master.py
+++
@@ -1,7 +0,0 @@
-#-----------------------------------------------------------------#
-# PANEl MASTER
-#-----------------------------------------------------------------#
-
-DATA_path = "C:/Users/wb519128/WBG/Sveta Milusheva - COVID 19 Results/"
-DATA_POC = DATA_path + "proof-of-concept/"
-DATA_panel = DATA_POC + "panel_indicators/"
