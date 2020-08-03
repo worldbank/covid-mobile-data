1f39d6b783ee0601cb79df99ea7e299d9e675502
LeonardoViotti <leonardoviotti@gmail.com>
2020-08-03 12:22:03 -0400

    Loading all indicators in dev panel draft.

--- data-panel/draft.py
+++ data-panel/draft.py
@@ -4,7 +4,7 @@ default_levels_dict = {1: [3],
                        2: [2,3],
                        3: [2,3],
                        4: ['country'],
-                       5: [2,3,'tower-cluster-harare', 'tower-cluster-bulawayo'],
+                       5: [2,3,'tower-tc_bulawayo-harare', 'tc_bulawayo'],
                        6: [3],
                        7: [2,3],
                        8: [2,3],
@@ -35,147 +35,70 @@ class panel_constructor:
         # 2. Unique subscribers per hour
         if 2 in self.indicators_list:
             self.i2 = i_indicator(num = 2,  index_cols = ['hour', 'region'])
-        # 3. Unique subscribers per day
-        if 3 in self.indicators_list:
-            if 3 in ilevels_dict[3]:
-                self.i3 = i_indicator(num = 3,  index_cols = ['day', 'region'])
-            if 2 in ilevels_dict[3]:
-                self.i3_2 = i_indicator(num = 3,  index_cols = ['day', 'region'], level = 2)
+        
+        # 3. Unique subscribers per day 
+        if (3 in self.indicators_list) & (3 in self.ilevels_dict[3]):
+            self.i3 = i_indicator(num = 3,  index_cols = ['day', 'region'])
+        if (3 in self.indicators_list) & (3 in self.ilevels_dict[3]):
+            self.i3_2 = i_indicator(num = 3,  index_cols = ['day', 'region'], level = 2)
+        
         # 4. Proportion of active subscribers
         # if 4 in self.indicators_list:
         #     self.i4
+        
         # 5 - Connection Matrix
-        if 5 in self.indicators_list:
-            if 3 in ilevels_dict[5]:
-                self.i5 = i_indicator(num = 3,  index_cols = ['day', 'region'])
-            if 2 in ilevels_dict[5]:
-                self.i5_2 = i_indicator(num = 3,  index_cols = ['day', 'region'], level = 2)
-            if 'tower-cluster-harare' in ilevels_dict[5]:
-                i5_tc_h = i_indicator(num = 5,  index_cols = ['connection_date', 'region_from', 'region_to'], level = 'tower-cluster-harare')
-            if 'tower-cluster-bulawayo' in ilevels_dict[5]:
-                i5_tc_b = i_indicator(num = 5,  index_cols = ['connection_date', 'region_from', 'region_to'], level = 'tower-cluster-bulawayo')
+        if (5 in self.indicators_list) & (3 in self.ilevels_dict[5]):
+            self.i5 = i_indicator(num = 3,  index_cols = ['day', 'region'])
+        if (5 in self.indicators_list) & (2 in self.ilevels_dict[5]):
+            self.i5_2 = i_indicator(num = 3,  index_cols = ['day', 'region'], level = 2)
+        if (5 in self.indicators_list) & ('tc-harare' in self.ilevels_dict[5]):
+            self.i5_tc_harare = i_indicator(num = 5,  index_cols = ['connection_date', 'region_from', 'region_to'], level = 'tc-harare')
+        if (5 in self.indicators_list) & ('tc_bulawayo' in self.ilevels_dict[5]):  
+            self.i5_tc_bulawayo = i_indicator(num = 5,  index_cols = ['connection_date', 'region_from', 'region_to'], level = 'tc_bulawayo')
+        
+        # 6. Unique subscribers per home location
         if 6 in self.indicators_list:
-            pass
-        if 7 in self.indicators_list:
-            pass
-        if 8 in self.indicators_list:
-            pass
-        if 9 in self.indicators_list:
-            pass
-        if 10 in self.indicators_list:
-            pass
-        if 11 in self.indicators_list:
-            pass
+            self.i6 = i_indicator(num = 6,  index_cols = ['week', 'home_region'])
+        
+        # 7. Mean and Standard Deviation of distance traveled per day (by home location)
+        if (7 in self.indicators_list) & (3 in self.ilevels_dict[7]):
+            self.i7 = i_indicator(num = 7,  index_cols =['day','home_region'], level = 3)
+        if (7 in self.indicators_list) & (2 in self.ilevels_dict[7]):
+            self.i7_2 = i_indicator(num = 7,  index_cols =['day','home_region'], level = 2)
+        
+        # 8. Mean and Standard Deviation of distance traveled per week (by home location)
+        if (8 in self.indicators_list) & (3 in self.ilevels_dict[8]):
+            self.i8 = i_indicator(num = 8,  index_cols =['week','home_region'], level = 3)
+        if (8 in self.indicators_list) & (2 in self.ilevels_dict[8]):
+            self.i8_2 = i_indicator(num = 8,  index_cols =['week','home_region'], level = 2)
+        # 9. Daily locations based on Home Region with average stay time and SD of stay time
+        if (9 in self.indicators_list) & (3 in self.ilevels_dict[9]):
+            self.i9 = i_indicator(num = 9,  index_cols =['day', 'region', 'home_region'], level = 3)
+        if (9 in self.indicators_list) & (2 in self.ilevels_dict[9]):
+            self.i9_2 = i_indicator(num = 9,  index_cols =['day', 'region', 'home_region'], level = 2)
+        
+        # 10. Simple OD matrix with duration of stay
+        if (10 in self.indicators_list) & (3 in self.ilevels_dict[10]):
+            self.i10 = i_indicator(num = 10,  index_cols =['day', 'region', 'region_lag'], level = 3)
+        if (10 in self.indicators_list) & (2 in self.ilevels_dict[10]):
+            self.i10_2 = i_indicator(num = 10,  index_cols =['day', 'region', 'region_lag'], level = 2)
+        
+        # 11. Monthly unique subscribers per home region
+        if (11 in self.indicators_list) & (3 in self.ilevels_dict[11]):
+            self.i11 = i_indicator(num = 11,  index_cols =['month', 'home_region'], level = 3)
+        if (11 in self.indicators_list) & (2 in self.ilevels_dict[11]):
+            self.i11_2 = i_indicator(num = 11,  index_cols =['month', 'home_region'], level = 2)
 
 
 bar = panel_constructor([5])
+bar.i11.data
 
-a = {1: [3], '2': [2,3],'4': ['country'], 5: [2,3,'harare-tc']}
+i_indicator(num = 5,  index_cols = ['connection_date', 'region_from', 'region_to'], level = 'tc_bulawayo')
 
-a[5]
+dic1 = {'Name': 'John', 'Time': 'morning'}
+'i'.join(map('_'.join, dic1.items()))
 
+# a = {1: [3], '2': [2,3],'4': ['country'], 5: [2,3,'harare-tc']}
 
+# a[5]
 
-
-
-
-
-
-
-# Clean panel as methods of the indicator class??
-
-
-# Define indicator class that contains data for multiple extractions
-class i_indicator:
-    """
-    This class contains information to load indicator files both
-    from our original indicators and externally created ones.
-    
-    load() method loads both datasets
-    clean() method removes missings from both datasets
-    """
-    def __init__(self, 
-                 num, 
-                 index_cols,
-                 level = 3,
-                 files_df = indicators_df):
-        # self.file_name = file_name
-        self.num = num
-        self.index_cols = index_cols
-        self.level = level
-        self.files_df = files_df
-        # Call methods when intializing
-        self.load()
-        self.clean()
-    # Load files
-    def load(self, full = True):
-        idx = (self.files_df['indicator'] == str(self.num)) & (self.files_df['level'] == str(self.level))
-        # Internal indicator
-        folder = self.files_df['path'][idx].iat[0]
-        file_name = self.files_df['file'][idx].iat[0] + '.csv'
-        self.data = pd.read_csv(folder + file_name)
-        # self.data = folder + file_name
-        # External indicators
-        if full:
-            folder = self.files_df['path_ecnt'][idx].iat[0]
-            file_name_03 = '2020_03_' + self.files_df['file'][idx].iat[0] + '.csv'
-            file_name_04 = '2020_04_' + self.files_df['file'][idx].iat[0] + '.csv'
-            file_name_05 = '2020_05_' + self.files_df['file'][idx].iat[0] + '.csv'
-            file_name_06 = '2020_06_' + self.files_df['file'][idx].iat[0] + '.csv'
-            self.data_e_03 = pd.read_csv(folder + file_name_03)
-            self.data_e_04 = pd.read_csv(folder + file_name_04)
-            self.data_e_05 = pd.read_csv(folder + file_name_05)
-            self.data_e_06 = pd.read_csv(folder + file_name_06)
-    # Clean indicators
-    def clean(self):
-        self.data = clean(self.data, self.index_cols)
-        self.data_e_03 = clean(self.data_e_03, self.index_cols)
-        self.data_e_04 = clean(self.data_e_04, self.index_cols)
-        self.data_e_05 = clean(self.data_e_05, self.index_cols)
-        self.data_e_06 = clean(self.data_e_06, self.index_cols)
-    # Internal merge function
-    # def out_merge(self, d1, d2, suffix, on = self.index_cols):
-    #     return d1.merge(d2, on = on, how = 'outer', suffixes=('', suffix))
-    
-    # Create panel with other data sets being added as columns. Gambiarra braba !Arrumar!
-    def create_panel(self, 
-                     time_var, 
-                     c_date_1 = np.datetime64(dt.date(2020, 3, 15)),
-                     c_date_2 = np.datetime64(dt.date(2020, 4, 1)),
-                     c_date_3 = np.datetime64(dt.date(2020, 5, 1)),
-                     c_date_4 = np.datetime64(dt.date(2020, 6, 1)) ):
-        # kwargs.setdefault('time_var', self.index_cols[0])
-        self.panel = self.data\
-            .merge(self.data_e_03,
-                   on = self.index_cols,
-                   how = 'outer',
-                   suffixes=('', '_03'))\
-            .merge(self.data_e_04,
-                   on = self.index_cols,
-                   how = 'outer',
-                   suffixes=('', '_04'))\
-            .merge(self.data_e_05,
-                   on = self.index_cols,
-                   how = 'outer',
-                   suffixes=('', '_05'))\
-            .merge(self.data_e_06,
-                   on = self.index_cols,
-                   how = 'outer',
-                   suffixes=('', '_06'))
-        # Create panel column
-        d1_bol = (self.panel[time_var].astype('datetime64')  >= c_date_1)
-        d2_bol = (self.panel[time_var].astype('datetime64')  >= c_date_2)
-        d3_bol = (self.panel[time_var].astype('datetime64')  >= c_date_3)
-        d4_bol = (self.panel[time_var].astype('datetime64')  >= c_date_4)
-        countvars =  list(set(self.data.columns) - set(self.index_cols))
-        for var in countvars:
-            varname = var + '_p'
-            # Base value as our indicator
-            self.panel[varname] = self.panel[var]
-            # Replace values based on dates
-            self.panel.loc[d1_bol, varname] = self.panel.loc[d1_bol, var + '_03'] 
-            self.panel.loc[d2_bol, varname] = self.panel.loc[d2_bol, var + '_04']
-            self.panel.loc[d3_bol, varname] = self.panel.loc[d3_bol, var + '_05']
-            self.panel.loc[d4_bol, varname] = self.panel.loc[d3_bol, var + '_06']
-        # Make sure order is fine
-        # self.panel.sort_values(self.index_cols)          
