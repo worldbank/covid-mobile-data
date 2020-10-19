3a9e7a45da9056426f71b99d2f2509b79bb74a92
LeonardoViotti <leonardoviotti@gmail.com>
2020-10-19 17:29:08 -0400

    Update app.R

--- dashboard-dataviz/dashboard/app.R
+++ dashboard-dataviz/dashboard/app.R
@@ -1,4 +1,4 @@
-# Zimbabwe Mobility Dashboard
+# Mobility Dashboard
 
 ##### ******************************************************************** #####
 # 1. PACKAGES AND SETUP ========================================================
@@ -9,16 +9,16 @@ options(rsconnect.max.bundle.size = 99999999999)
 
 #### Setting directory so will work locally
 if (Sys.info()[["user"]] == "robmarty") {
-  setwd("~/Documents/Github/covid-mobile-data/dashboard-dataviz/zimbabwe_dashboard")
+  setwd("~/Documents/Github/covid-mobile-data/dashboard-dataviz/dashboard")
 }
 
 if (Sys.info()[["user"]] == "WB521633") {
-  setwd("C:/Users/wb521633/Documents/Github/covid-mobile-data/dashboard-dataviz/zimbabwe_dashboard"
+  setwd("C:/Users/wb521633/Documents/Github/covid-mobile-data/dashboard-dataviz/dashboard"
   )
 }
 
 if (Sys.info()[["user"]] == "wb519128") {
-  setwd("C:/Users/wb519128/GitHub/covid-mobile-data/dashboard-dataviz/zimbabwe_dashboard"
+  setwd("C:/Users/wb519128/GitHub/covid-mobile-data/dashboard-dataviz/dashboard"
   )
 }
 
@@ -175,7 +175,7 @@ ui_main <- fluidPage(
   navbarPage(
     theme = shinytheme("flatly"), # journal
     collapsible = TRUE,
-    title = "Zimbabwe",
+    title = "Country",
     
     id = "nav",
     
