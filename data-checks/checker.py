4b3c5679cb69336e2e2e4696e64b04da30ee1f4a
rz233 <43656240+rz233@users.noreply.github.com>
2020-12-10 17:13:08 -0500

    Update checker.py

--- data-checks/checker.py
+++ data-checks/checker.py
@@ -69,13 +69,28 @@ class checker:
         files_bol = all([os.path.isfile(self.path + '/' + self.ind_dict[key]) for key in self.ind_dict.keys()])
         assert files_bol,"Some indicators don't exist. Check defaults or set ind_dict"
         
-        # Indicator default column names 
+        # Indicator default column names
+        # Construct column names of each indicator as key-value pairs 
+        # Column names of some indicators do not have keys yet. Will continue working on it.
         if col_names_dict is None:
             self.col_names_dict = {
-                    'Time': 'hour',
-                    'Geography': 'region',
-                    'Count': 'count' 
-                   }
+                    'i1_col_names': {'Time':'hour', 'Geography':'region','Count':'count'},
+                    'i2_col_names': {'Time':'hour','Geography':'region','Count':'count'},
+                    'i3_col_names': {'Time':'day','Geography':'region','Count':'count'},
+                    'i4_col_names': ('day','count','percent_active'),
+                    'i5_col_names': {'Time':'connection_date','Geography_01':'region_from','Geography_02':'region_to',
+                                     'Subcrib_Count':'subscriber_count',
+                                     'OD_Count':'od_count','Total_Count':'total_count'},
+                    'i6_col_names': ('week','home_region','count'),
+                    'i7_col_names': ('home_region','day','mean_distance','stdev_distance'),
+                    'i8_col_names': ('home_region','week','mean_distance','stdev_distance'),
+                    'i9_col_names': ('day','region','home_region','mean_duration','stdev_duration','count'),
+                    'i10_col_names': ('day','region','region_lag','total_duration_destination',
+                                      'avg_duration_destination','count_destination','stddev_duration_destination'
+                                      'total_duration_origin','avg_duration_origin','count_origin',
+                                      'stddev_duration_origin'),
+                    'i11_col_names': ('month','home_region','count')
+        }
         # Otherwise specify dict manually
         else:
             self.col_names_dict = col_names_dict
@@ -87,7 +102,8 @@ class checker:
         # Run data loading and processing methods
         self.load_indicators()
         self.run_aggregations()
-    # ---------------------------------------------------------
+
+   # ---------------------------------------------------------
     # Load indicator files
     def load_indicators(self):
         # Loading function
@@ -117,7 +133,7 @@ class checker:
     # Aggregated i1 versions
     def run_aggregations(self):
         # Missing region remove function
-        def remove_missings(df, regionvar = self.col_names_dict['Geography'], missing_values = self.missing_values):
+        def remove_missings(df, regionvar = self.col_names_dict['i1_col_names']['Geography'], missing_values = self.missing_values):
             return df[~df[regionvar].isin(missing_values)]
         
         # Create data sets with time indexes and fill blanks with 0s
@@ -134,40 +150,43 @@ class checker:
         
         # Indicator 1
         self.i1_hour = remove_missings(self.i1)\
-            .groupby(['date',  self.col_names_dict['Time']])\
-            .agg({self.col_names_dict['Geography'] : pd.Series.nunique ,
-                self.col_names_dict['Count'] : np.sum})\
+            .groupby(['date', self.col_names_dict['i1_col_names']['Time']])\
+            .agg({self.col_names_dict['i1_col_names']['Geography'] : pd.Series.nunique ,
+                self.col_names_dict['i1_col_names']['Count'] : np.sum})\
             .reset_index()\
-            .sort_values(['date', self.col_names_dict['Time']])\
-            .rename(columns = {self.col_names_dict['Geography'] : 'n_regions'})
+            .sort_values(['date', self.col_names_dict['i1_col_names']['Time']])\
+            .rename(columns = {self.col_names_dict['i1_col_names']['Geography'] : 'n_regions'})
         self.i1_date = remove_missings(self.i1)\
             .groupby('date')\
-            .agg({self.col_names_dict['Geography'] : pd.Series.nunique ,
-                self.col_names_dict['Count'] : np.sum})\
+            .agg({self.col_names_dict['i1_col_names']['Geography'] : pd.Series.nunique ,
+                self.col_names_dict['i1_col_names']['Count'] : np.sum})\
             .reset_index()\
             .sort_values(['date'])\
-            .rename(columns = {self.col_names_dict['Geography'] : 'n_regions'})
-            
+            .rename(columns = {self.col_names_dict['i1_col_names']['Geography'] : 'n_regions'})
         # Complete dates
         self.i1_date = time_complete(self.i1_date, 'date')
-        self.i1_hour = time_complete(self.i1_hour, self.col_names_dict['Time'], 'H')
-        # Indicator 5
-        #i5_nmissing = remove_missings(remove_missings(self.i5,'region_from' ), 'region_to')
-        #self.i5_date = i5_nmissing\
-        #.groupby('date')\
-        #.agg({'region_from' : pd.Series.nunique ,
-        #      'region_to' : pd.Series.nunique,
-        #      'total_count' : np.sum})\
-        #.reset_index()\
-        #.sort_values('date')
+        self.i1_hour = time_complete(self.i1_hour, self.col_names_dict['i1_col_names']['Time'], 'H')
         
-        #self.i5_date = time_complete(self.i5_date, 'date')
+        
+        # Indicator 5
+        i5_nmissing = remove_missings(remove_missings(self.i5,self.col_names_dict['i5_col_names']['Geography_01']), 
+                                      self.col_names_dict['i5_col_names']['Geography_02'])
+        self.i5_date = i5_nmissing\
+        .groupby('date')\
+        .agg({self.col_names_dict['i5_col_names']['Geography_01'] : pd.Series.nunique ,
+              self.col_names_dict['i5_col_names']['Geography_02'] : pd.Series.nunique,
+              self.col_names_dict['i5_col_names']['Total_Count'] : np.sum})\
+        .reset_index()\
+        .sort_values('date')
+        
+        self.i5_date = time_complete(self.i5_date, 'date')
     
     # ---------------------------------------------------------
     # Plots
     
     def plot_i1_count(self, show = True):
-        fig = go.Figure(data=go.Scatter(x=self.i1_date.index, y=self.i1_date[self.col_names_dict['Count']]))
+        fig = go.Figure(data=go.Scatter(x=self.i1_date.index, 
+                                        y=self.i1_date[self.col_names_dict['i1_col_names']['Count']]))
         fig.update_layout(title_text="Indicator 1: Total number of transactions.")
         
         file_name = self.outputs_path + '/' + 'i1_count.html'
@@ -187,7 +206,8 @@ class checker:
             fig.show()
     
     def plot_i5_count(self, show = True):
-        fig = go.Figure(data=go.Scatter(x=self.i5_date.index, y=self.i5_date['total_count']))
+        fig = go.Figure(data=go.Scatter(x=self.i5_date.index, 
+                                        y=self.i5_date[self.col_names_dict['i5_col_names']['Total_Count']]))
         fig.update_layout(title_text="Indicator 5: Total number of movements.")
         
         file_name = self.outputs_path + '/' + 'i5_count.html'
@@ -197,8 +217,10 @@ class checker:
             fig.show()
     def plot_i5_region_count(self, show = True):
         fig = go.Figure()
-        fig.add_trace(go.Scatter(x=self.i5_date.index, y=self.i5_date['region_to'], marker=dict(color="blue"), name = 'Destination regions'))
-        fig.add_trace(go.Scatter(x=self.i5_date.index, y=self.i5_date['region_from'], marker=dict(color="red"), name = 'Origin regions'))
+        fig.add_trace(go.Scatter(x=self.i5_date.index, 
+                                 y=self.i5_date[self.col_names_dict['i5_col_names']['Geography_02']], marker=dict(color="blue"), name = 'Destination regions'))
+        fig.add_trace(go.Scatter(x=self.i5_date.index, 
+                                 y=self.i5_date[self.col_names_dict['i5_col_names']['Geography_01']], marker=dict(color="red"), name = 'Origin regions'))
         fig.update_layout(title_text="Indicator 5: Number of unique regions.")
         
         file_name = self.outputs_path + '/' + 'i5_region_count.html'
@@ -208,7 +230,7 @@ class checker:
             fig.show()
     
     def plot_region_missings(self, show = True):
-        n_missing = self.i1[self.col_names_dict['Geography']].isin(self.missing_values).sum() 
+        n_missing = self.i1[self.col_names_dict['i1_col_names']['Geography']].isin(self.missing_values).sum() 
         labels = ['Missing region','Non-missing region']
         values = [n_missing, len(self.i1) - n_missing]
         
@@ -226,8 +248,8 @@ class checker:
         self.plot_region_missings()
         self.plot_i1_count()
         self.plot_i1_n_regions()
-        #self.plot_i5_count()
-        #self.plot_i5_region_count()
+        self.plot_i5_count()
+        self.plot_i5_region_count()
         
     # USAGE OUTILERS: Indicator wards and days with towers down
     def usage_outliers(self, htrahshold = None):
@@ -235,7 +257,7 @@ class checker:
         if htrahshold is None:
              htrahshold = self.htrahshold
         # Number of hours with transactions per region day
-        hours_per_day = data.groupby([self.col_names_dict['Geography'], 'date']).size()
+        hours_per_day = data.groupby([self.col_names_dict['i1_col_names']['Geography'], 'date']).size()
         hours_per_day = hours_per_day.reset_index() # ger regions to be a column
         hours_per_day.columns = ['region', 'date', 'hcount']
     
@@ -275,5 +297,4 @@ class checker:
 
 
 
-
-
+# plotly.offline.plot(fig, filename = data + '/' + 'filename.html', auto_open=False)
